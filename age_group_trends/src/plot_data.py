dc9eea60c5ad68547edc32f7876e601eaf84de71
martroben <mart.roben@gmail.com>
2025-05-31 14:00:51 +0300

    add plot_data.py

---
+++ age_group_trends/src/plot_data.py
@@ -0,0 +1,16 @@
+
+
+##########
+# Inputs #
+##########
+
+root_dir = "age_group_trends"
+age_group_path = "data/clean/age_group.csv"
+regeneration_cutting_path = "data/clean/regeneration_cutting.csv"
+
+
+#############
+# Read data #
+#############
+
+
